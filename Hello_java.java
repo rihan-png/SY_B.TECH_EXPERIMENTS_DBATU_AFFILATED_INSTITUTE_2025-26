d3d70224e11c03c2d59b0bbf42b7fcab23fb9622
RIHAN MAHAMADASIF BAGWAN <rihanbagwan74@gmail.com>
2026-03-14 22:01:14 +0530

    Create Hello_java.java
    File Naming: In Java, the filename must match the class name. So, this code must be saved in a file named Main.java.
    
    Case Sensitivity: Java is picky. main is not the same as Main, and system (lowercase) will throw an error.
    
    The Semicolon: Don't forget the ; at the end of the print statement! It’s the "period" at the end of a Java sentence.

---
+++ Hello_java.java
@@ -0,0 +1,8 @@
+public class Main {  //Defines a class named Main. In Java, every line of code must live inside a class. The public keyword means it's accessible from other classes.
+  public static void main(String[] args) { //This is the entry point of the program. When you run the code, the Java Virtual Machine (JVM) looks for this exact signature to start execution.
+    System.out.println("Hello World"); //The command to print text to the console. ln stands for "line," meaning it adds a new line after the text.
+  }
+ String[] args - This allows the program to accept "command-line arguments" (extra information passed during startup) as an array of strings.
+
+
+   
